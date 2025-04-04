d7acaffb520b5dc724d082a293e221ab001d052a
Nyks <redgo3666@gmail.com>
2025-04-04 13:09:13 +0800

    Finished and renamed problem 8

--- python_batch_6/file8.py
+++
@@ -1,2 +0,0 @@
-﻿#Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
-#Iterate through each character, converting lowercase to uppercase and vice versa using isupper(), lower(), and upper().
---
+++ python_batch_6/swapcase_function.py
@@ -0,0 +1,12 @@
+﻿#Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+#Iterate through each character, converting lowercase to uppercase and vice versa using isupper(), lower(), and upper().
+test_string = "woNYOUNG aRM 123"
+result = ""
+for char in test_string:
+    if char.isupper():
+        result += char.lower()
+    elif char.islower():
+        result += char.upper()
+    else:
+        result += char
+print(f"'{test_string}' swapcased: '{result}'")
