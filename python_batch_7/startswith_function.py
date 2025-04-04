218e8b8927ca7850352fa8f061add97d226026ec
Nyks <redgo3666@gmail.com>
2025-04-04 14:40:12 +0800

    Finished and renamed problem 5

---
+++ python_batch_7/startswith_function.py
@@ -0,0 +1,6 @@
+﻿#Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+#Check if a string starts with a specific prefix by comparing the beginning slices.
+string = "hellowonyoung"
+prefix = "hello"
+is_startswith = len(prefix) <= len(string) and string[:len(prefix)] == prefix
+print(is_startswith)
