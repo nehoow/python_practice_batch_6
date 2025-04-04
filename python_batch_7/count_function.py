69e6d8c8f8b9165fcbd0bbea771f499fb17cb6a2
Nyks <redgo3666@gmail.com>
2025-04-04 14:46:33 +0800

    Finished and renamed problem 8

--- python_batch_7/count_function.py
+++ python_batch_7/count_function.py
@@ -1,2 +1,9 @@
 ﻿#Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 #I will count the occurrences of a substring by iterating and comparing slices.
+string = "wonyoung is so pretty like she is so pretty"
+substring = "pretty"
+count = 0
+for index in range(len(string) - len(substring) + 1):
+    if string[index:index + len(substring)] == substring:
+        count += 1
+print(count)
