cc2f8117dbed8b096c190f60f9810fb85e07b0fd
Nyks <redgo3666@gmail.com>
2025-04-04 13:07:41 +0800

    Finished and renamed problem 6

--- python_batch_6/ljust_function.py
+++ python_batch_6/ljust_function.py
@@ -1,2 +1,12 @@
 ﻿#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 # i would print the filler characters until desired length of the difference of desiredlength and the string.
+test_string = "wonyoung"
+length = 15
+fillchar = '~'
+
+if len(test_string) >= length:
+    print(test_string)  # No padding needed
+else:
+    padding_length = length - len(test_string)
+    padding = fillchar * padding_length
+    print(test_string + padding)
