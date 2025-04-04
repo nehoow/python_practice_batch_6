4fa7b4530b9451c3a731603a66bb14b5f2bfbf7b
Nyks <redgo3666@gmail.com>
2025-04-04 14:29:51 +0800

    Finished and renamed problem 1

--- python_batch_7/file1.py
+++
@@ -1,1 +0,0 @@
-﻿#Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
---
+++ python_batch_7/rstrip_function.py
@@ -0,0 +1,8 @@
+﻿#Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+#Trim trailing spaces from a string by iterating backwards and slicing.
+string = "hello wonyoung   "
+lastindex = len(string) - 1
+while lastindex >= 0 and string[lastindex].isspace():
+    lastindex -= 1
+result = string[:lastindex + 1]
+print(result)
