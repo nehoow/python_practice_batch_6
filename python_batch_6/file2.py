11b998a0eecc31c80fcc6be73546847a5c392211
Nyks <redgo3666@gmail.com>
2025-04-04 13:03:21 +0800

    Finished and renamed problem 2

--- python_batch_6/file2.py
+++
@@ -1,3 +0,0 @@
-﻿#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
-#ill use .startswith() to know if the prefix is there
-#if the prefix ill use the len of the prefix and starts the index there to "remove" it
---
+++ python_batch_6/remove_prefix.py
@@ -0,0 +1,13 @@
+﻿#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+#ill use .startswith() to know if the prefix is there
+#if the prefix ill use the len of the prefix and starts the index there to "remove" it
+input_string = "wonyoung"
+prefix = "won"
+
+if input_string.startswith(prefix):
+    result = input_string[len(prefix):]
+else:
+    result = input_string
+
+print(f"Original string: {input_string}")
+print(f"Result: {result}")
