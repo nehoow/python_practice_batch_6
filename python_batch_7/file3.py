983f140d0a0e4d7c187f569822763d948fdecfaf
Nyks <redgo3666@gmail.com>
2025-04-04 14:36:16 +0800

    Add a possibile solution to problem 3

--- python_batch_7/file3.py
+++ python_batch_7/file3.py
@@ -1,1 +1,2 @@
 ﻿#Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+#Convert all characters to uppercase by iterating and converting lowercase using swapcase
