b53df673b44448905c09480f04878c4e63108a38
Nyks <redgo3666@gmail.com>
2025-04-04 14:43:06 +0800

    Finished and renamed problem 7

--- python_batch_7/file7.py
+++ python_batch_7/file7.py
@@ -1,2 +1,6 @@
 ﻿#Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 #add leading zeros to reach target width by calculating needed zeros
+string = "123"
+width = 5
+result = "0" * (width - len(string)) + string
+print(result)
