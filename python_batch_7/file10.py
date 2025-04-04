2baa6a2b6db9351d3a81bafda49fe8cd4529186a
Nyks <redgo3666@gmail.com>
2025-04-04 14:52:10 +0800

    Finished and renamed problem 10

--- python_batch_7/file10.py
+++ python_batch_7/file10.py
@@ -1,2 +1,10 @@
 ﻿#Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 #Find the last index of a substring by iterating backwards and comparing slices, breaking on first match.
+string = "wonyoung is teasing me again i kinda like it"
+substring = "like"
+last_index = -1
+for index in range(len(string) - len(substring), -1, -1):
+    if string[index:index + len(substring)] == substring:
+        last_index = index
+        break
+print(last_index)
