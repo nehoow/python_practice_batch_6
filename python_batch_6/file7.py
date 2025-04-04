7ca1e97c99f8be2ccb856e19a61241ea5c741612
Nyks <redgo3666@gmail.com>
2025-04-04 13:08:13 +0800

    Added the problem and possible solution to problem 7

--- python_batch_6/file7.py
+++ python_batch_6/file7.py
@@ -1,1 +1,2 @@
-﻿print(f'This is file {7}')
+﻿#Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+#i will calculate left and right padding to evenly distribute space around the string to center it within the specified length, and print the resulting padded string.
