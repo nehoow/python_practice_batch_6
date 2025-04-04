2c1e29cb2859570206a072677c2f26b8690d4060
Nyks <redgo3666@gmail.com>
2025-04-04 13:09:48 +0800

    Finished and renamed problem 9

---
+++ python_batch_6/capitalize_function.py
@@ -0,0 +1,8 @@
+﻿#Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+#Capitalize the first character and lowercase the rest of the string using upper() and lower().
+test_string = "woNYOUNG aRM 123"
+if test_string:
+    result = test_string[0].upper() + test_string[1:].lower()
+else:
+    result = ""
+print(f"'{test_string}' capitalized: '{result}'")
--- python_batch_6/file9.py
+++
@@ -1,2 +0,0 @@
-﻿#Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
-#Capitalize the first character and lowercase the rest of the string using upper() and lower().
