69d1cf5570ec7cbe513788d13d7ee05457b6c743
Nyks <redgo3666@gmail.com>
2025-04-04 14:51:49 +0800

    Add a possibile solution to problem 10

--- python_batch_7/file10.py
+++ python_batch_7/file10.py
@@ -1,1 +1,2 @@
 ﻿#Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+#Find the last index of a substring by iterating backwards and comparing slices, breaking on first match.
