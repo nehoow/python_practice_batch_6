620aa152ab2df7e9dfd54f8a93f3971784327931
Nyks <redgo3666@gmail.com>
2025-04-04 13:01:16 +0800

    Added a possible solution using pseudocode to the problem 1

--- python_batch_6/file1.py
+++ python_batch_6/file1.py
@@ -1,1 +1,3 @@
-﻿print(f'This is file {1}')
+﻿#Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+#loop each character of the string, if character is space the loop will continue until it isnt a space then it will return the index number.
+#print result using the  index we got from the loop and use it as a starting point, where there are no leading spaces.
