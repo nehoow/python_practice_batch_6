92f88b30b53d8d7d4e83ac426c40c8cb40a7a48a
Nyks <redgo3666@gmail.com>
2025-04-04 14:41:24 +0800

    Finished and renamed problem 6

--- python_batch_7/rjust_function.py
+++ python_batch_7/rjust_function.py
@@ -1,2 +1,6 @@
 ﻿#Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
 # Right-justify the string with spaces by adding leading spaces based on width given.
+string = "haewon"
+width = 10
+result = " " * (width - len(string)) + string
+print(result)
