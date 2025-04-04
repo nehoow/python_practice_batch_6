1dffff0e1b6ed6ceceb85f55c088a1f8d962c8ee
Nyks <redgo3666@gmail.com>
2025-04-04 14:39:08 +0800

    Add a possibile solution to problem 5

--- python_batch_7/file5.py
+++ python_batch_7/file5.py
@@ -1,1 +1,2 @@
 ﻿#Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+#Check if a string starts with a specific prefix by comparing the beginning slices.
