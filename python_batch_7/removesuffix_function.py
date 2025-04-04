637d702d0f78020d364b7ab62820fa3f687ba2b0
Nyks <redgo3666@gmail.com>
2025-04-04 14:32:33 +0800

    Finished and renamed problem 2

---
+++ python_batch_7/removesuffix_function.py
@@ -0,0 +1,9 @@
+﻿#Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+#Remove a specific suffix if it exists by checking the end and slicing if it matches.
+string = "wonyoungarm"
+suffix = "arm"
+if string.endswith(suffix):
+    result = string[:-len(suffix)]
+else:
+    result = string
+print(result)
