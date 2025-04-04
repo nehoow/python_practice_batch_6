7634482aed2e9a251d64bb600a4acb187ccc8a51
Nyks <redgo3666@gmail.com>
2025-04-04 13:02:50 +0800

    Added the problem and pseudocode to problem 2

--- python_batch_6/file2.py
+++ python_batch_6/file2.py
@@ -1,1 +1,3 @@
-﻿print(f'This is file {2}')
+﻿#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+#ill use .startswith() to know if the prefix is there
+#if the prefix ill use the len of the prefix and starts the index there to "remove" it
