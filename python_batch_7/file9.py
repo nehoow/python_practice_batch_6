4a9db174076f9cab9a519d9e33f74ed1b5850940
Nyks <redgo3666@gmail.com>
2025-04-04 14:49:54 +0800

    Finished and renamed problem 9

--- python_batch_7/file9.py
+++
@@ -1,2 +0,0 @@
-﻿#Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
-#Find the first index of a substring by iterating and comparing slices, breaking on first match.
---
+++ python_batch_7/index_function.py
@@ -0,0 +1,10 @@
+﻿#Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+#Find the first index of a substring by iterating and comparing slices, breaking on first match.
+string = "wonyoung smells so nice"
+substring = "smell"
+index_found = -1
+for index in range(len(string) - len(substring) + 1):
+    if string[index:index + len(substring)] == substring:
+        index_found = index
+        break
+print(index_found)
