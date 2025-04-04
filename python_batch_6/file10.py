aae52526a5b733f4137ca514e6871316ec2fdc1e
Nyks <redgo3666@gmail.com>
2025-04-04 13:10:10 +0800

    Added the problem and possible solution to problem 10

--- python_batch_6/file10.py
+++ python_batch_6/file10.py
@@ -1,1 +1,3 @@
-﻿print(f'This is file {10}')
+﻿#Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+# Iterate through characters, capitalizing the first letter of each word (after a non-alphabetic character) and lowercasing the rest, 
+# using isalpha(), upper(), and lower().
