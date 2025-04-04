f384df86a6bb8854ba1226a074d39ecc72e08f5d
Nyks <redgo3666@gmail.com>
2025-04-04 13:04:19 +0800

    Finished and renamed problem 3

--- python_batch_6/lowercase_string.py
+++ python_batch_6/lowercase_string.py
@@ -1,3 +1,12 @@
 ﻿#Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 # ill use a loop the iterates every character 
 # if the character iscapitalize ill can swap it 
+test_string = "wonyouNg aRM"
+result = ""
+for char in test_string:
+    if char.isupper():
+        lowercase = char.swapcase()
+        result += lowercase
+    else:
+        result += char
+print(result)
