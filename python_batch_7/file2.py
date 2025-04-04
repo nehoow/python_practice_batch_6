9a43cd19bec85dafe88fb79854c650af6c28278a
Nyks <redgo3666@gmail.com>
2025-04-04 14:30:51 +0800

    Add a possibile solution to problem 2

--- python_batch_7/file2.py
+++ python_batch_7/file2.py
@@ -1,1 +1,2 @@
 ﻿#Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+#Remove a specific suffix if it exists by checking the end and slicing if it matches.
