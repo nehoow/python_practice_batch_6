40eba32e2cbdac9278117a3292ded4b19cebb1b0
Nyks <redgo3666@gmail.com>
2025-04-04 13:06:53 +0800

    Finished and renamed problem 5

---
+++ python_batch_6/endswith_function.py
@@ -0,0 +1,14 @@
+﻿#Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+# i will check if test_string is long enough to contain the suffix and if it is, i will check the trailing portion of the test_string if it matches the suffix.
+test_string = "wonyoung.mp4"
+suffix = ".mp4"
+
+string_length = len(test_string)
+suffix_length = len(suffix)
+
+if string_length >= suffix_length and test_string[string_length - suffix_length:] == suffix:
+    result = True
+else:
+    result = False
+
+print(f"'{test_string}' ends with '{suffix}': {result}")
--- python_batch_6/file5.py
+++
@@ -1,2 +0,0 @@
-﻿#Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
-# i will check if test_string is long enough to contain the suffix and if it is, i will check the trailing portion of the test_string if it matches the suffix.
