c22c6872124e2a6879fadf5096dfa04437b60288
Nyks <redgo3666@gmail.com>
2025-04-04 13:03:57 +0800

    Added the problem and pseudocode to problem 3

--- python_batch_6/file3.py
+++ python_batch_6/file3.py
@@ -1,1 +1,3 @@
-﻿print(f'This is file {3}')
+﻿#Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+# ill use a loop the iterates every character 
+# if the character iscapitalize ill can swap it 
