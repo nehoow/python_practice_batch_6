738afb76b88d2cd0460bbbe89d9e6ecbccd891ad
Nyks <redgo3666@gmail.com>
2025-04-04 13:06:27 +0800

    Added the problem and possible solution to problem 5

--- python_batch_6/file5.py
+++ python_batch_6/file5.py
@@ -1,1 +1,2 @@
-﻿print(f'This is file {5}')
+﻿#Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+# i will check if test_string is long enough to contain the suffix and if it is, i will check the trailing portion of the test_string if it matches the suffix.
