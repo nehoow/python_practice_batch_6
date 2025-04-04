896b56c22a5aa6ed4742cec2b0a34de55cf90019
Nyks <redgo3666@gmail.com>
2025-04-04 14:36:41 +0800

    Finished and renamed problem 3

--- python_batch_7/upper_function.py
+++ python_batch_7/upper_function.py
@@ -1,2 +1,10 @@
 ﻿#Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 #Convert all characters to uppercase by iterating and converting lowercase using swapcase
+string = "HellO wonYounG"
+result = ""
+for char in string:
+    if char.islower():
+        result += char.swapcase()
+    else:
+        result += char
+print(result)
