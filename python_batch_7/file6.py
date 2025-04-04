54a876112946926cbd0dee7ca0ef23bbb11bdc5c
Nyks <redgo3666@gmail.com>
2025-04-04 14:40:52 +0800

    Add a possibile solution to problem 6

--- python_batch_7/file6.py
+++ python_batch_7/file6.py
@@ -1,1 +1,2 @@
 ﻿#Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+# Right-justify the string with spaces by adding leading spaces based on width given.
