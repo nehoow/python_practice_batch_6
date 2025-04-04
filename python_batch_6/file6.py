d7ff929458b7a7e28b7d522cd513b66e3d8c2a3e
Nyks <redgo3666@gmail.com>
2025-04-04 13:07:24 +0800

    Added the problem and possible solution to problem 6

--- python_batch_6/file6.py
+++ python_batch_6/file6.py
@@ -1,1 +1,2 @@
-﻿print(f'This is file {6}')
+﻿#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+# i would print the filler characters until desired length of the difference of desiredlength and the string.
