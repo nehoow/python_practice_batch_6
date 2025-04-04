7ab361b931a71d6e63da69ea741f6c2f42bbb98d
Nyks <redgo3666@gmail.com>
2025-04-04 13:09:32 +0800

    Added the problem and possible solution to problem 9

--- python_batch_6/file9.py
+++ python_batch_6/file9.py
@@ -1,1 +1,2 @@
-﻿print(f'This is file {9}')
+﻿#Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+#Capitalize the first character and lowercase the rest of the string using upper() and lower().
